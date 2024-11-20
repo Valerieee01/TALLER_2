7a32a4af5c95f0c2c30a0a1a7595bd5cffe2f386
Valerieee01 <zharmel.afa@gmail.com>
2024-11-19 21:21:31 -0500

    completo a medias

--- Ejercicio_9/Ejercicio_9.js
+++ Ejercicio_9/Ejercicio_9.js
@@ -1,3 +1,6 @@
-const number = [10,8,6,1,3,4,2];
-const ejecucion = dropWhile(number, (x) => x < 5);
-console.log(ejecucion);
+const _ = require('underscore');
+console.log(_);
+
+// const number = [10,8,6,1,3,4,2];
+// const ejecucion = _.dropWhile(number, (x) => x < 5);
+// console.log(ejecucion);
